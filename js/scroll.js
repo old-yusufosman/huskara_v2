e4a7da701dd54cd6a1368d95fcf9847b62357283
Yusuf Osman <yusuf.o@gmail.com>
2016-09-04 15:50:45 +0100

    before major change

--- js/scroll.js
+++ js/scroll.js
@@ -67,7 +67,7 @@ var tween = new TimelineMax ()
 // build scene
 var scene = new ScrollMagic.Scene({triggerElement: "#parrot-trigger", duration: 400})
     .setTween(tween)
-    .addIndicators() // add indicators (requires plugin)
+    //.addIndicators() // add indicators (requires plugin)
     .addTo(controller);
 
 var tween = new TimelineMax ()
